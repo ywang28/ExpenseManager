7eab7331449157a638f2ec4d28e1915d195313a3
Yalin Wang <ywang28@ncsu.edu>
2016-01-10 17:40:25 -0600

    add expense category buttons and expense input UI

--- app/src/main/java/com/myandroid/expensemanager/MainActivity.java
+++ app/src/main/java/com/myandroid/expensemanager/MainActivity.java
@@ -2,6 +2,9 @@ package com.myandroid.expensemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addExpense(View view) {
+        TextView totalExpense = (TextView) findViewById(R.id.total_expense);
+        EditText input = (EditText) findViewById(R.id.input);
+
+        float existingExpense = Float.valueOf(totalExpense.getText().toString());
+        float newExpense = Float.valueOf(input.getText().toString());
+
+        totalExpense.setText(String.valueOf(existingExpense + newExpense));
+
+        input.setText("");
     }
 }
